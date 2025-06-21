c81bed9ae4c92a916b786a73cfc6b0fcdb0c4785
Mr.Pan <panwq.email@qq.com>
2025-06-21 16:02:38 +0800

    chore(数据库): 包管理工具替换为npm,集成sqlite
    集成typeorm、sqlite和swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  const config = new DocumentBuilder()
+    .setTitle('中轴精控管理系统API')
+    .setDescription('中轴精控项目后端API文档')
+    .setVersion('1.0')
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, documentFactory);
+
+  await app.listen(3000);
 }
 bootstrap();
